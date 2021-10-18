cbefce19f787d89d75f767835a307dbcc95a23fa
Akhilchary <61978652+Akhilchary@users.noreply.github.com>
2021-10-18 22:03:31 +0530

    routes in topbar

--- frontend/src/components/Topbar/Topbar.jsx
+++ frontend/src/components/Topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "./Topbar.css";
-import menuIcon from "../../images/menuIcon.svg";
-import searchIcon from "../../images/searchIcon.svg";
-import logo from "../../images/logo.png";
+// import menuIcon from "../../images/menuIcon.svg";
+// import searchIcon from "../../images/searchIcon.svg";
+// import logo from "../../images/logo.png";
 function Topbar(){
     return(
         <>
@@ -12,12 +12,14 @@ function Topbar(){
                     <h2 className="name">The Compendium</h2>
                 </div>
                 <div className="menu-items">
-                    <p>Home</p>
-                    <p>About Us</p>
-                    <p>Articles</p>
-                    <p>Gallery</p>
-                    <p>Spotlight</p>
-                    <p>Placements</p>
+                    <p><Link to="/">Home </Link> </p>
+                    <p><Link to="aboutus"> About Us</Link> </p>
+                    <p><Link to="articleslist">Articles </Link> </p>
+                    <p><Link to="gallery">Gallery </Link> </p>
+                    <p><Link to="/"> Spotlight</Link> </p>
+                    <p><Link to="placements"> Placements</Link> </p>
+
+                    
                     {/* <img className="search-icon" src={searchIcon} alt="" /> */}
                 </div>
             </div>
