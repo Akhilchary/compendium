d161c067fdc15093631e441727187114b1883fa2
ANKIT11RAO <ankit123goku@gmail.com>
2021-10-19 13:47:40 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -17,21 +17,21 @@ function Homebody(){
     return(
     <div className="wholebody">
         <div className="body1">
-            {/* <img src={sitting} alt="" className="sitting"  /> */}
+            <img src={sitting} alt="" className="sitting"  />
             <div className="social">
                 
                 <img className="linked-in" src={linkedin}   alt="" />
                 <img className="instagram" src={instagram}  alt="" />
             </div>
-            <div className="body1">
+            <div className="bodyone">
                 <p className="tagline">The News and Publication Society of IARE</p>
                 <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae sed magna malesuada in interdum massa. Odio vel interdum ultrices tellus.</p>
-                <h1 className="body1subscribe">Subscribe</h1>
+                <h1 className="body1-subscribe">Subscribe</h1>
             </div>
         </div>
         
         <div className="body2">
-            <p className="articles">Articles</p>
+            <p className="articles">Articles.</p>
             <button className="findbutton" type="button">Find Out More</button>
             <div className="twoarticles">
                 <div className="article1">
@@ -46,30 +46,37 @@ function Homebody(){
 
         <div className="body3">
             
-            <p className="aboutus" >About Us</p>
+            <p className="aboutus" >About Us.</p>
             <div className="body3logo">
                 <img src={logo} alt="" />
             </div>
             <p className="aboutus-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. convallis diam egestas.Morbi congue sed mattis. Morbi congue sed mattis.</p>
             <button className="findbutton1" type="button">Find Out More</button>
         </div>
+
+
         <div className="body4">
-            <p className="NewspaperArchives" >Newspaper Archives</p>
+            <p className="NewspaperArchives" >Newspaper Archives.</p>
             <p className="NewspaperArchives-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. convallis diam egestas.Morbi congue sed mattis. Morbi congue sed mattis.</p>
             <h1 className="number01">01</h1>
             <p className="firstedition">First Edition</p>
         </div>
+
+
         <div className="body5">
-            <p className="Spotlight" >Spotlight</p>
+            <p className="Spotlight" >Spotlight.</p>
             <p className="Spotlight-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. convallis diam egestas.Morbi congue sed mattis. Morbi congue sed mattis.</p>
             <button className="findbutton2" type="button">Find Out More</button>
         </div>
+
+
         <div className="body6">
-            <p className="Subscribe" >Subscribe</p>
+            <p className="Subscribe" >Subscribe.</p>
             <img className="subscribeimage" src={subscribe} alt='Un available' />
             <div className="subscribe-content">
                 <h1 className="newsletter">Newsletter Signup</h1>
                 <p className="email">Your Email Address</p>
+                <div  className="arrow2"><img src={arrow} alt="Un available" /></div>
                 <hr />
             </div>
         </div>
