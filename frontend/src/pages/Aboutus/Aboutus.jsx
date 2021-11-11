739930a88f0667489030dbf43335a33df48f7c0a
Akhilchary <61978652+Akhilchary@users.noreply.github.com>
2021-11-11 16:19:56 +0530

    changes

--- frontend/src/pages/Aboutus/Aboutus.jsx
+++ frontend/src/pages/Aboutus/Aboutus.jsx
@@ -13,36 +13,36 @@ import { Link } from "react-router-dom";
 function Aboutusbody(){
     return (
         <div>
-            <div class="body1">
-                <h1 class="about">About.</h1>
-                <img class="logo" src={Logo} alt="" />
-                <p className="about-text1">m ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.Lorem ipsum dolor sit amet,</p>
-                <p className="about-text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.sit amet, consectetur adipiscing elit. Aliquam dictum gravida et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.Lorem ipsum dolor sit amet,</p>
+            <div className="body1">
+                <h1 className="about">About.</h1>
+                <img className="logo" src={Logo} alt="" />
+                <p classNameName="about-text1">m ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.Lorem ipsum dolor sit amet,</p>
+                <p classNameName="about-text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.sit amet, consectetur adipiscing elit. Aliquam dictum gravida et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.Lorem ipsum dolor sit amet,</p>
             </div>
-            <div className="body2">
-                <p className="team">The Team</p>
-                <img class="lines" src={Lines} alt="" />
-                <Link to="/founder"><img class="Founder" src={Founder} alt="" /></Link>
-                <div className="foundertextarea">
-                    <p class="Foundertext">Founder</p>
-                    <p className="name">Anusha Vajha</p>
+            <div classNameName="body2">
+                <p classNameName="team">The Team</p>
+                <img className="lines" src={Lines} alt="" />
+                <Link to="/founder"><img className="Founder" src={Founder} alt="" /></Link>
+                <div classNameName="foundertextarea">
+                    <p className="Foundertext">Founder</p>
+                    <p classNameName="name">Anusha Vajha</p>
                 </div>
             </div>
-            <div className="body3">
-                <Link to="/creativedirector"><img class="Creativedirector" src={Creativedirector} alt="" /></Link>
-                <div className="creativedirectortextarea">
-                    <p class="CreativeDirectortext">Creative Director</p>
-                    <p className="name">Sharwann Solanki</p>
+            <div classNameName="body3">
+                <Link to="/creativedirector"><img className="Creativedirector" src={Creativedirector} alt="" /></Link>
+                <div classNameName="creativedirectortextarea">
+                    <p className="CreativeDirectortext">Creative Director</p>
+                    <p classNameName="name">Sharwann Solanki</p>
                 </div>
-                <Link to="/president"><img class="president" src={President} alt="" /></Link>
-                <div className="presidenttextarea">
-                    <p class="Presidenttext">President</p>
-                    <p className="name">Rahul Sattarapu</p>
+                <Link to="/president"><img className="president" src={President} alt="" /></Link>
+                <div classNameName="presidenttextarea">
+                    <p className="Presidenttext">President</p>
+                    <p classNameName="name">Rahul Sattarapu</p>
                 </div>
-                <Link to="/managingdirector"><img class="Managingdirector" src={Managingdirector} alt="" /></Link>
-                <div className="managingdirectortextarea">
-                    <p class="ManagingDirectortext">Managing Director</p>
-                    <p className="name">Sachin Pisipati</p>
+                <Link to="/managingdirector"><img className="Managingdirector" src={Managingdirector} alt="" /></Link>
+                <div classNameName="managingdirectortextarea">
+                    <p className="ManagingDirectortext">Managing Director</p>
+                    <p classNameName="name">Sachin Pisipati</p>
                 </div>
             </div>
         </div>
--- frontend/src/pages/Aboutus/president.jsx
+++ frontend/src/pages/Aboutus/president.jsx
@@ -6,12 +6,12 @@ import Cross from "../../images/Vector 35.png";
 import { Link } from "react-router-dom";
 function Presidentbody(){
     return(
-        <div class="Presidentbody">
-           <Link to="/aboutus"><img class="Cross" src={Cross} alt="" /></Link>
-            <img class="Presidentpic" src={Presidentpic} alt="" />
-            <p class="teammembersname">Rahul Sattarapu</p>
-            <p class="presidentname">President</p>
-            <p className="presidentdescription">An achiever, dreamer and fine leader. Introducing to you the new President of The Compendium, Rahul (Sattarapu Rahul). He aims for perfection in all his ventures. An ordinary person with a skill to practise perfection is what he is. Previously, he had served tirelessly as the Vice President of the club. He is one with high spirits and never lets a teammate down. Always a sight for calm and composure, he leads the team with all his heart. He has proved multiple times that nothing is bigger than The Compendium to him. Otherwise, he is just an ordinary student with a weird combination of maturity and kiddish mentality which changes dynamically. He’s a person who knows exactly what he wants and almost always successfully can convey it to the team and get it done. </p>
+        <div className="Presidentbody">
+           <Link to="/aboutus"><img className="Cross" src={Cross} alt="" /></Link>
+            <img className="Presidentpic" src={Presidentpic} alt="" />
+            <p className="teammembersname">Rahul Sattarapu</p>
+            <p className="presidentname">President</p>
+            <p classNameName="presidentdescription">An achiever, dreamer and fine leader. Introducing to you the new President of The Compendium, Rahul (Sattarapu Rahul). He aims for perfection in all his ventures. An ordinary person with a skill to practise perfection is what he is. Previously, he had served tirelessly as the Vice President of the club. He is one with high spirits and never lets a teammate down. Always a sight for calm and composure, he leads the team with all his heart. He has proved multiple times that nothing is bigger than The Compendium to him. Otherwise, he is just an ordinary student with a weird combination of maturity and kiddish mentality which changes dynamically. He’s a person who knows exactly what he wants and almost always successfully can convey it to the team and get it done. </p>
         </div>
     );
 }
