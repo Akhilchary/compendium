a62996861c7eadaefa12cd4567692dcfc5efedeb
ANKIT11RAO <ankit123goku@gmail.com>
2021-11-16 22:48:58 +0530

    Update Slider.js

--- frontend/src/pages/Gallery/Slider.js
+++ frontend/src/pages/Gallery/Slider.js
@@ -67,9 +67,27 @@ export default function Slider() {
                         ></div>
                     ))}
             </div>
-        </div>
+            <div className="footer-4">
 
-        <Footer />
+            <p className="soc-p">Social.</p>
+            <img className="linkdin-ic" src={linkedIcon} alt="" />
+            <img className="insta-ic" src={instaIcon} />
+
+            <div className="footer-nav">
+                <h3>Navigate</h3>
+                <div className="footNav-content">
+                    <p>About us</p>
+                    <p>Articles</p>
+                    <p>Gallery</p>
+                    <p>Spotlight</p>
+                    <p>Placements</p>
+                </div>
+            </div>
+
+            <p className="copyright">&copy;2021 TheCompendium  </p>
+            
+            </div> 
+        </div>
       </>
     )
 }
